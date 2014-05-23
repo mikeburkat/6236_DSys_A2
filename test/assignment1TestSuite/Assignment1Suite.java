c5e00f3ffa77c70f79f74bfa6b553bd47013bc5f
mikeburkat <mike.burkat@gmail.com>
2014-05-22 23:47:09 -0400

    added test suite

---
+++ test/assignment1TestSuite/Assignment1Suite.java
@@ -0,0 +1,14 @@
+package assignment1TestSuite;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Assignment1Suite {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
